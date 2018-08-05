818a2717977476cc5cf1192bc0ed2f1791f484ac
Kari O'Neal <triadfirewood@gmail.com>
2018-08-05 20:01:49 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,7 @@ end
 
 def all_supplies_in_holidays(holiday_hash)
    holiday_hash.each do |holiday, supplies|
-     puts "#{holiday.caplitalize}: #{supplies}"
+     puts "#{holiday.caplitalize!}: #{supplies}"
       
   end
   # iterate through holiday_hash and print items such that your readout resembles:
