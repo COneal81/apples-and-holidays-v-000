9293a48cd69620001c200944584bcc9bf7b310d6
Kari O'Neal <triadfirewood@gmail.com>
2018-08-05 20:32:56 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -92,7 +92,10 @@ def all_supplies_in_holidays(holiday_hash)
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+    array_list = []
+    holiday_hash.each do |season, holiday|
+      holiday.each do |holiday_event, supply|
+        array_list << 
 end
 
 
