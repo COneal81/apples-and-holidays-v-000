62429e6a4af7193ebf0873408a9dc5073a5e6913
Kari O'Neal <triadfirewood@gmail.com>
2018-08-05 20:11:16 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,8 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
-   holiday_hash.each do |holiday, supplies|
+   holiday_hash.each do |season, holiday|
+     puts "#{season.to_s"
      supplies.each do |attribute, value|
      puts "#{attribute}: #{value}"
     end
