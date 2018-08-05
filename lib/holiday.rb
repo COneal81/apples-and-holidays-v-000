cb1fb7a3f5cffe29fcceaa7cf99392cb3b63e446
Kari O'Neal <triadfirewood@gmail.com>
2018-08-05 19:50:10 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,7 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  return holiday_hash[:winter][:summer][:fall][:spring].keys.values
+  puts holiday_hash[:winter][:summer][:fall][:spring].keys.values
       # binding.pry
   
   # iterate through holiday_hash and print items such that your readout resembles:
