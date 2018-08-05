6db5edcabc3bf5e3574bd05356a6575394e31726
Kari O'Neal <triadfirewood@gmail.com>
2018-08-05 19:33:32 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].values
-  binding.pry
+  # binding.pry
   # return an array of all of the supplies that are used in the winter season
 
 end
