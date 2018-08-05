4a20923d91e0d7a38c6dc04a397044193254444d
Kari O'Neal <triadfirewood@gmail.com>
2018-08-05 20:28:45 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,7 @@ def all_supplies_in_holidays(holiday_hash)
    holiday_hash.each do |season, holiday|
      puts "#{season.to_s.capitalize}:"
      holiday.each do |holiday_event, supplies|
-     puts " #{holiday_event.to_s.split("_").each {|letter| 
+     puts "  #{holiday_event.to_s.split("_").each {|letter| 
      letter.capitalize!}.join(" ")}: #{supplies.join(", ")}"
       end
     end
